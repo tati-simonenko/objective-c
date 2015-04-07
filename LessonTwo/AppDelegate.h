1775192e2ff1ea08829c622678abf508ea1cdd54
Tatyana <tatyana.simonenko@gmail.com>
2015-04-08 01:04:33 +0400

    l2

---
+++ LessonTwo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LessonTwo
+//
+//  Created by T on 02.04.15.
+//  Copyright (c) 2015 T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
