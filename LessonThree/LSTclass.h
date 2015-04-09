f9a760eb73b26cd267864734384e665a0c900e93
Tatyana <tatyana.simonenko@gmail.com>
2015-04-09 14:52:54 +0400

    class

---
+++ LessonThree/LSTclass.h
@@ -0,0 +1,13 @@
+//
+//  LSTclass.h
+//  LessonThree
+//
+//  Created by T on 09.04.15.
+//  Copyright (c) 2015 T. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LSTclass : NSObject
+
+@end
---
+++ LessonThree/LSTclass.m
@@ -0,0 +1,13 @@
+//
+//  LSTclass.m
+//  LessonThree
+//
+//  Created by T on 09.04.15.
+//  Copyright (c) 2015 T. All rights reserved.
+//
+
+#import "LSTclass.h"
+
+@implementation LSTclass
+
+@end
