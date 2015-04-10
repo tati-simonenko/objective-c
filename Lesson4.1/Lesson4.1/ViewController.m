cf28bd18616fe18dc9bee143c6a938959dd63b54
Tatyana <tatyana.simonenko@gmail.com>
2015-04-10 23:59:45 +0400

    lesson4, push button

---
+++ Lesson4.1/Lesson4.1/ViewController.m
@@ -0,0 +1,99 @@
+//
+//  ViewController.m
+//  Lesson4.1
+//
+//  Created by T on 10.04.15.
+//  Copyright (c) 2015 T. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (nonatomic, strong) NSMutableArray * arrayBase;
+@property (nonatomic, strong) NSArray * arrayAmount;
+@property (nonatomic, strong) NSArray * arrayCharacters;
+
+
+@end
+
+@implementation ViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    NSString * stringAmount = @"245,4565,453,735,297,4721,849";
+    NSString * stringCharacters = @"Разумных,Ленивых,Упорных,Яростных,Серьезных,Трагических,Благоразумных";
+    
+    self.arrayBase = [NSMutableArray array];
+    
+    self.arrayAmount = [stringAmount componentsSeparatedByString:@","];
+    self.arrayCharacters = [stringCharacters componentsSeparatedByString:@","];
+    
+    
+    for (int i = 0; i < self.arrayAmount.count; i++) {
+        
+        NSDictionary * dict = [[NSDictionary alloc] initWithObjectsAndKeys:
+                               [self.arrayAmount objectAtIndex:i], @"amount",
+                               [self.arrayCharacters objectAtIndex:i], @"character", nil];
+        
+        [self.arrayBase addObject:dict];
+        
+    }
+    
+    NSLog(@"self.arrayBase %@", self.arrayBase);
+    
+    
+    
+}
+
+- (void) testMethod {
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+#pragma mark - UITableViewDelegate
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    
+    return self.arrayBase.count ;
+    
+    
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    
+    static NSString * simpleTableIdentifier = @"Cell";
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    
+    
+    cell.textLabel.text = [[self.arrayBase objectAtIndex:indexPath.row]objectForKey:@"amount"];
+    cell.detailTextLabel.text = [[self.arrayBase objectAtIndex:indexPath.row]objectForKey:@"character"];
+    
+    return cell;
+    
+}
+
+
+- (void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+}
+
+
+@end
