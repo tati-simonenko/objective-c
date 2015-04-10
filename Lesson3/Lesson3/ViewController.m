bcc43a89ef91cf3c75af77383becb1b71c3c5b34
Tatyana <tatyana.simonenko@gmail.com>
2015-04-10 13:48:07 +0400

    lesson3-homework__v2

--- Lesson3/Lesson3/ViewController.m
+++ Lesson3/Lesson3/ViewController.m
@@ -23,38 +23,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    //<<<<<<< Updated upstream
-    //    NSString * stringPrices = @"325,1013,99";
-    //    NSString * stringValues = @"Тетрадь,Телевизор,Утюг";
-    //
-    //    self.arrayPrices = [stringPrices componentsSeparatedByString:@","];
-    //    self.arrayValues = [stringValues componentsSeparatedByString:@","];
-    //
-    //
-    //
-    //
-    //
-    //    self.arrayM = [NSMutableArray array];
-    //
-    //    NSInteger i = 17;
-    //
-    //    NSDictionary * dict = [[NSDictionary alloc] initWithObjectsAndKeys:
-    //                           @"string1", @"str1",
-    //                           @"string2", @"str2",
-    //                           @"string3", @"str3",
-    //                           @(i), @"int", nil];
-    //
-    //    NSLog(@"dict %@", dict);
-    //=======
+
     NSString * stringAmount = @"245,4565,453,735,297,4721,849";
-    NSString * stringCharacters = @"Разумных,Ленивых,Упорных,Стеснительных,Очаровательных,Серьезных,Честных";
-    //  NSString * stringBalances = @"24,3,256,453,2301,14000260";
+    NSString * stringCharacters = @"Разумных,Ленивых,Упорных,Яростных,Серьезных,Трагических,Благоразумных";
     
     self.arrayBase = [NSMutableArray array];
     
     self.arrayAmount = [stringAmount componentsSeparatedByString:@","];
     self.arrayCharacters = [stringCharacters componentsSeparatedByString:@","];
-    // self.arrayBalances = [stringBalances componentsSeparatedByString:@","];
     
     
     for (int i = 0; i < self.arrayAmount.count; i++) {
@@ -62,7 +38,6 @@
         NSDictionary * dict = [[NSDictionary alloc] initWithObjectsAndKeys:
                                [self.arrayAmount objectAtIndex:i], @"amount",
                                [self.arrayCharacters objectAtIndex:i], @"character", nil];
-        //                               [self.arrayBalances objectAtIndex:i], @"balance",
         
         [self.arrayBase addObject:dict];
         
@@ -70,15 +45,7 @@
     
     NSLog(@"self.arrayBase %@", self.arrayBase);
     
-    //    NSInteger i = 17;
-    
-    //    NSDictionary * dict = [[NSDictionary alloc] initWithObjectsAndKeys:
-    //                           @"string1", @"str1",
-    //                           @"string2", @"str2",
-    //                           @"string3", @"str3",
-    //                           @(i), @"int", nil];
-    
-    
+
     
 }
 
@@ -90,7 +57,6 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
@@ -106,21 +72,7 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    //<<<<<<< Updated upstream
-    //=======
-    
-    //    Player *player = (self.players)[indexPath.row];
-    //
-    //    UILabel *nameLabel = (UILabel *)[cell viewWithTag:100];
-    //    nameLabel.text = player.name;
-    //
-    //    UILabel *gameLabel = (UILabel *)[cell viewWithTag:101];
-    //    gameLabel.text = player.game;
-    //
-    //    UIImageView *ratingImageView = (UIImageView *)[cell viewWithTag:102];
-    //    ratingImageView.image = [self imageForRating:player.rating];
-    
-    
+
     
     static NSString * simpleTableIdentifier = @"Cell";
     UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
@@ -128,31 +80,15 @@
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
     }
     
-    //    cell.textLabel.text = [self.arrayValues objectAtIndex:indexPath.row];
-    //    cell.detailTextLabel.text = [self.arrayPrices objectAtIndex:indexPath.row];
-    //    return cell;
-    
-    //}
+
     
     cell.textLabel.text = [[self.arrayBase objectAtIndex:indexPath.row]objectForKey:@"amount"];
     cell.detailTextLabel.text = [[self.arrayBase objectAtIndex:indexPath.row]objectForKey:@"character"];
-    //    cell.detailTextLabel.text = [[self.arrayBase objectAtIndex:indexPath.row]objectForKey:@"balance"];
     
     return cell;
     
 }
-//- (UIImage *)imageForRating:(int)rating
-//{
-//    switch (rating) {
-//        case 1: return [UIImage imageNamed:@"1StarSmall"];
-//        case 2: return [UIImage imageNamed:@"2StarsSmall"];
-//        case 3: return [UIImage imageNamed:@"3StarsSmall"];
-//        case 4: return [UIImage imageNamed:@"4StarsSmall"];
-//        case 5: return [UIImage imageNamed:@"5StarsSmall"];
-//    }
-//    return nil;
-//}
-//>>>>>>> Stashed changes
+
 
 - (void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath
 {
