1f0730df23cb2eb95d438341d6d502af8eeb6917
Tatyana <tatyana.simonenko@gmail.com>
2015-04-10 23:22:06 +0400

    lesson4.npr

---
+++ Lesson4/Lesson4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson4
+//
+//  Created by T on 10.04.15.
+//  Copyright (c) 2015 T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson4/Lesson4/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Lesson4
+//
+//  Created by T on 10.04.15.
+//  Copyright (c) 2015 T. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
