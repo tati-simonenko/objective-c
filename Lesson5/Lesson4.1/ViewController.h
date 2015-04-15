1922c101fec5b16e98109e314fd61d1d9e4023c4
Tatyana <tatyana.simonenko@gmail.com>
2015-04-15 16:55:01 +0400

    new project

---
+++ Lesson5/Lesson4.1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson4.1
+//
+//  Created by T on 10.04.15.
+//  Copyright (c) 2015 T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+
+@end
+
---
+++ Lesson5/Unnamed.h
@@ -0,0 +1,16 @@
+//
+//  Unnamed.h
+//  Lesson4.1
+//
+//  Created by T on 11.04.15.
+//  Copyright (c) 2015 T. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Unnamed : NSObject
+
+@property (nonatomic, strong) NSString * model;
+@property (nonatomic, strong) NSString * year;
+
+@end
